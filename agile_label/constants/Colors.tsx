a95677c79777a660434e61be5ea2230ed8250cb8
watson404v <appletabetai1101@gmail.com>
2025-08-18 15:50:37 +0900

    その他の画面を追加

--- agile_label/constants/Colors.tsx
+++ agile_label/constants/Colors.tsx
@@ -1,16 +1,19 @@
 export const Colors = {
   // プライマリカラー
   primary: '#3365D8',
+  tint: '#3365D8',
   
   // セカンダリカラー
   // secondary: '#5856D6',
   
   // 背景色
   background: '#EDF2F7',
+  cardBackground: '#FFFFFF',
   card: '#FFFFFF',
   
   // テキストカラー
   text: '#0F1419',
+  textSecondary: '#8E8E93',
   
   // 状態カラー
   success: '#7DEA17',
