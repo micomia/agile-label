ca02089cad3ac5fec5775d616c7964b35f86de60
watson404v <appletabetai1101@gmail.com>
2025-08-25 18:14:49 +0900

    developmentビルドかつandroidで共有したときにzipでエクスポートできるように変更

---
+++ agile_label/types/react-native-zip-archive.d.ts
@@ -0,0 +1,4 @@
+declare module 'react-native-zip-archive' {
+  export function zip(source: string, target: string): Promise<string>;
+  export function unzip(source: string, target: string): Promise<string>;
+}
